82606f3e3abf7ae9a84905cee09fb2ca958bb2a4
asaf abek <asaf.abek@gmail.com>
2025-11-03 09:45:32 +0200

    route default angular

--- angular/client/src/app/app.routes.ts
+++ angular/client/src/app/app.routes.ts
@@ -1,9 +1,10 @@
 import { Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
+import { AngularWrapperComponent } from './angular-wrapper.component';
 
 export const routes: Routes = [
   {
-    path:'home',component: HomeComponent
+    path:'',component: AngularWrapperComponent
   },
   {
     path: 'react',
@@ -18,7 +19,7 @@ export const routes: Routes = [
     redirectTo: 'home',
     pathMatch: 'full'
   },
-  {path:'**',component: HomeComponent}
+  {path:'**',component: AngularWrapperComponent}
 
   
 ];
